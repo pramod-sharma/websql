f2c897c50c2d7541e8a2b015eb2d1913f922b53b
pramod-sharma <pramod@targetingmantra.com>
2015-08-20 21:30:15 +0530

    Initial Commit

---
+++ websql.js
@@ -0,0 +1,50 @@
+WebSQLDB = {
+  defaultDBSize: (1024 * 1024),
+
+  create: function(name, version, options = {}) {
+            var description = options.description || name,
+                dbSize      = options.dbSize || this.defaultDBSize,
+                callback    = options.callback,
+                db;
+
+            if callback
+              db = openDataBase(name, version, description, dbSize, callback);
+            else
+              db = openDataBase(name, version, description, dbSize);
+
+            webSQLDB    = new WebSQLDB;
+            webSQLDB.db = db;
+
+            return webSQLDB;
+          },
+
+  this.prototype = {
+    createTable: function(name, options = {}) {
+                   var table = this.executeSql(WebSQLTable.createSQL(name, options));
+
+                   this.tables.push(table);
+                 },
+
+    executeSql: function(sqlStatement) {
+                  this.db.transaction(function (transaction) {
+                     transaction.executeSql(sqlStatement);
+                  });
+                }
+  }
+}
+
+WebSQLTable = {
+  createSQL: function(tableName, options = {}) {
+            var dbFields = [];
+            $.map(options, function(value, key) {
+              dbFields.push("'" + key + value + "'");
+            });
+
+            return 'CREATE TABLE IF NOT EXISTS' + tableName + '(' + dbFields.join(', ') + ')';
+          },
+
+  this.prototype = {
+
+  }
+
+}
